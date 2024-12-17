b73c3be160aa431b2b0d93d002cbdaed8f2be7bc
Sudhakar <sudhakarpotamsetti@gmail.com>
2024-12-17 23:39:08 +0530

    db , api validations

--- src/app.js
+++ src/app.js
@@ -1,76 +1,103 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middlewares/auth");
-const app = express();
 const connectDB = require("./config/database");
+const app = express();
 const User = require("./models/user");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
+  //   Creating a new instance of the User model
   const user = new User(req.body);
+
   try {
     await user.save();
-    res.send("user saved");
+    res.send("User Added successfully!");
   } catch (err) {
-    console.error("user signup unsuccessful :" + err.message);
+    res.status(400).send("Error saving the user:" + err.message);
   }
 });
 
-//Feed api - GET all users
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
 
+  try {
+    console.log(userEmail);
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+
+    // const users = await User.find({ emailId: userEmail });
+    // if (users.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(users);
+    // }
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
 app.get("/feed", async (req, res) => {
   try {
-    const users = await User.find({}); // returns all users {emailId:'email'} finds array of users with that email
+    const users = await User.find({});
     res.send(users);
   } catch (err) {
-    res.status(400).send("Something went wrong");
+    res.status(400).send("Something went wrong ");
   }
 });
 
-app.delete("/user-delete", async (req, res) => {
-  const { userId } = req.body;
-  console.log("user delete");
-
+// Detele a user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
   try {
-    const user = await User.findByIdAndDelete(userId, {
-      returnDocument: "before",
-    }); // We can also pass { __id : userId } or we can use the following approach
+    const user = await User.findByIdAndDelete({ _id: userId });
+    //const user = await User.findByIdAndDelete(userId);
 
-    // const userDelete = await User.findOneAndDelete(
-    //   { _id: userId },
-    //   { returnDocument: "before" }
-    // );
-    console.log(user);
-    res.send("User deleted");
+    res.send("User deleted successfully");
   } catch (err) {
-    res.status(400).send("Something went wrong");
+    res.status(400).send("Something went wrong ");
   }
 });
 
-app.patch("/user-update", async (req, res) => {
-  // whatever the fields we got from request body are updated if we got any new fields other than schema they are ignored
-  const user = req.body;
+// Update data of the user
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
   try {
-    const updatedUser = await User.findByIdAndUpdate(user.userId, user, {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
+      runValidators: true,
     });
-    console.log(updatedUser);
-    res.send("user updated successfully");
+    console.log(user);
+    res.send("User updated successfully");
   } catch (err) {
-    res.status(400).send("Something went wrong");
+    res.status(400).send("UPDATE FAILED:" + err.message);
   }
 });
 
 connectDB()
   .then(() => {
-    app.listen(3000, () => {
-      console.log("server listening on port 3000");
+    console.log("Database connection established...");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777...");
     });
   })
   .catch((err) => {
-    console.error("error in db connection");
+    console.error("Database cannot be connected!!");
   });
-
-app.use("/", (err, req, res, next) => {
-  if (err) res.status(500).send("something went wrong");
-});
