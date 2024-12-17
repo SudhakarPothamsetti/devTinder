13777e434c7868a5f9205cdd179b785901ceb5f6
Sudhakar <sudhakarpotamsetti@gmail.com>
2024-12-17 18:02:55 +0530

    user delete , update apis

--- src/app.js
+++ src/app.js
@@ -16,6 +16,51 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//Feed api - GET all users
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({}); // returns all users {emailId:'email'} finds array of users with that email
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user-delete", async (req, res) => {
+  const { userId } = req.body;
+  console.log("user delete");
+
+  try {
+    const user = await User.findByIdAndDelete(userId, {
+      returnDocument: "before",
+    }); // We can also pass { __id : userId } or we can use the following approach
+
+    // const userDelete = await User.findOneAndDelete(
+    //   { _id: userId },
+    //   { returnDocument: "before" }
+    // );
+    console.log(user);
+    res.send("User deleted");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user-update", async (req, res) => {
+  // whatever the fields we got from request body are updated if we got any new fields other than schema they are ignored
+  const user = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(user.userId, user, {
+      returnDocument: "after",
+    });
+    console.log(updatedUser);
+    res.send("user updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     app.listen(3000, () => {
